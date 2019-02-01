8d454887ead631c0afa236860dc14301e0f5e615
mgauthier@digitalriver.com <mgauthier@digitalriver.com>
2019-02-01 16:35:42 -0600

    committing the index and js file despite everything being in the index file for now.

---
+++ react.js
@@ -0,0 +1,7 @@
+// create my first React component. These usually take the form of JavaScript classes
+
+class Hello extends React.Component {
+    render() {
+        return <h1>Yay for React!</h1>;
+    }
+}
